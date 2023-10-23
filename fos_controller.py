f14747def3dfec3f10cb158a3999578ef61cda4f
Dominik Szalanski <ds@cryptostruct.com>
2023-10-23 09:17:14 +0200

    added logging and config

--- fos_controller.py
+++ fos_controller.py
@@ -22,7 +22,7 @@ async def main(args):
     global running_fos
     global shutdown
     uri = f"ws://{args.strategyserver}/ws/v1"
-    print(f"Connection to Strategyserver ({uri})")
+    logger.log(logging.INFO,f"Connection to Strategyserver ({uri})")
     async with websockets.connect(uri) as websocket:
         login_dict = {'op':'login','origin':'fos_controller_service'}
         await websocket.send(json.dumps(login_dict))
@@ -40,6 +40,7 @@ async def main(args):
                     running_fos[process_id] = True
                     asyncio.ensure_future(spawn_fos(fos_start_message_dict, uri, process_id))
             except (websockets.exceptions.ConnectionClosed,websockets.exceptions.ConnectionClosedError,websockets.exceptions.ConnectionClosedOK):
+                logger.log(logging.WARN,f"connection to {uri} closed")
                 shutdown = True
         unsubscribe_dict = {'op':'unsubscribe','channel':'fos'}
         try: 
@@ -53,7 +54,8 @@ async def main(args):
 async def spawn_fos(fos_args:dict, ws_address:str, process_id:str):
     global running_fos
     
-    process = await asyncio.create_subprocess_shell(f'./{binary_name} -p {process_id} -r {fos_args.get("reference_instrument_symbol").lower()} -t {fos_args.get("trading_instrument_symbol")} -s {ws_address} -c {fos_args.get("monitor_order_channel_name")} --api-key {fos_args.get("api_key")} --api-secret {fos_args.get("api_secrect")} --connection-count {connection_count} --connect-delay-s {connect_delay_s} > {process_id}', stdin = PIPE, stdout = PIPE, stderr = STDOUT)
+    logger.log(logging.INFO,f"spawn new fos {process_id}")
+    process = await asyncio.create_subprocess_shell(f'./{binary_name} -p {process_id} -r {fos_args.get("reference_instrument_symbol").lower()} -t {fos_args.get("trading_instrument_symbol")} -s {ws_address} -c {fos_args.get("monitor_order_channel_name")} --api-key {fos_args.get("api_key")} --api-secret {fos_args.get("api_secrect")} --connection-count {connection_count} --connect-delay-s {connect_delay_s} > {process_id}.log', stdin = PIPE, stdout = STDOUT, stderr = STDOUT)
     async with websockets.connect(ws_address) as websocket:
         login_dict = {'op':'login','origin':process_id}
         await websocket.send(json.dumps(login_dict))
@@ -64,24 +66,34 @@ async def spawn_fos(fos_args:dict, ws_address:str, process_id:str):
         timeout = False
         while not shutdown and not timeout:
             try:
-                await asyncio.wait_for(websocket.recv(), timeout=60)
+                await asyncio.wait_for(websocket.recv(), timeout=ws_monitoring_timeout)
             except asyncio.TimeoutError:
                 timeout = True
+                logger.log(logging.INFO,f"no message for {ws_monitoring_timeout} received for process {process_id}")
     process.send_signal(signal.SIGTERM)
+    logger.log(logging.INFO,f"despawn fos {process_id}")
     running_fos.pop(process_id, None)
 
 def killall_fos():
+    logger.log(logging.INFO,f"killall fos")
     subprocess.run(f'kill -15 $(pgrep {binary_name})', shell = True, executable="/bin/bash")
 
-def handler(signum):
-    print(f'Signal handler called with signal {signum}')
+def soft_signal_handler(signum):
+    logger.log(logging.INFO,f'soft signal handler called with signal {signum}')
     global shutdown
     shutdown = True
 
+def hard_signal_handler(signum):
+    killall_fos()
+    logger.log(logging.WARN,f'hard signal handler called with signal {signum}')
+    exit(1)
+
 async def mainFunction(args):
     main_event_loop = asyncio.get_event_loop()
-    main_event_loop.add_signal_handler(signal.SIGTERM, functools.partial(handler, signal.SIGTERM))
-    main_event_loop.add_signal_handler(signal.SIGINT, functools.partial(handler, signal.SIGINT))
+    main_event_loop.add_signal_handler(signal.SIGTERM, functools.partial(soft_signal_handler, signal.SIGTERM))
+    main_event_loop.add_signal_handler(signal.SIGINT, functools.partial(soft_signal_handler, signal.SIGINT))
+    main_event_loop.add_signal_handler(signal.SIGKILL, functools.partial(hard_signal_handler, signal.SIGINT))
+    main_event_loop.add_signal_handler(signal.SIGILL, functools.partial(hard_signal_handler, signal.SIGINT))
     await asyncio.create_task(main(args))
 
 
@@ -105,6 +117,12 @@ if __name__ == '__main__':
             ws_monitoring_timeout = ws_monitoring_timeout if config_infile.get('ws_monitoring_timeout') is None else config_infile.get('ws_monitoring_timeout')
             connection_count = connection_count if config_infile.get('connection_count') is None else config_infile.get('connection_count')
             connect_delay_s = connect_delay_s if config_infile.get('connect_delay_s') is None else config_infile.get('connect_delay_s')
+    logfile = 'fos_controller.log'
+    loglevel = logging.INFO
+    logging.basicConfig(filename=logfile,filemode='w+',level=loglevel,format='%(asctime)s %(levelname)s - %(name)s - %(message)s')
+    logger = logging.getLogger()
+    logger.addHandler(RotatingFileHandler(logfile, maxBytes=1024*1024*5, backupCount=1))
+    
     killall_fos()
     asyncio.run(mainFunction(args))
 
